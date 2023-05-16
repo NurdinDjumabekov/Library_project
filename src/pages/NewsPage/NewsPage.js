c1859f169d7902e03b2ad4f143c64cce85d2878b
nthngspcl <atajpp6@gmail.com>
2023-05-16 22:47:20 +0600

    16may

--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -1,40 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styles from './NewsPage.module.css'
-import { NavLink } from 'react-router-dom'
+import News from '../../components/News'
 
 function NewsPage() {
-    const [newsList,setNewsList] = useState([
-        {name:"Hoвость 1", date: '01-04-2023', time: "17:46"},
-        {name:"Hoвость 2", date: '15-04-2023', time: "17:12"},
-        {name:"Hoвость 3", date: '15-04-2023', time: "17:13"},
-        {name:"Hoвость 4", date: '12-04-2023', time: "11:45"},
-        {name:"Hoвость 5", date: '10-04-2023', time: "12:26"},
-        {name:"Hoвость 6", date: '01-04-2023', time: "17:46"},
-        {name:"Hoвость 7", date: '15-04-2023', time: "17:12"},
-        {name:"Hoвость 8", date: '15-04-2023', time: "17:13"},
-        {name:"Hoвость 9", date: '12-04-2023', time: "11:45"},
-        {name:"Hoвость 10", date: '10-04-2023', time: "12:26"}])
-    let sortedNewList = newsList.sort((a,b) => b.time.localeCompare(a.time) )
-    sortedNewList = sortedNewList.sort((a,b) => new Date(...b.date.split('-').reverse()) - new Date(...a.date.split('-').reverse()))
-    let time = new Date()
-    console. log(time)
-    useEffect(() => {
-        window.scrollTo(0,0)},[NavLink])
+    const newsList = [{name:"Hoвость 1", date: '01-04-2023', time: "17:46"},
+    {name:"Hoвость 2", date: '15-04-2023', time: "17:12"},
+    {name:"Hoвость 3", date: '15-04-2023', time: "17:13"},
+    {name:"Hoвость 4", date: '12-04-2023', time: "11:45"},
+    {name:"Hoвость 5", date: '10-04-2023', time: "12:26"},
+    {name:"Hoвость 6", date: '01-04-2023', time: "17:46"},
+    {name:"Hoвость 7", date: '15-04-2023', time: "17:12"},
+    {name:"Hoвость 8", date: '15-04-2023', time: "17:13"},
+    {name:"Hoвость 9", date: '12-04-2023', time: "11:45"},
+    {name:"Hoвость 10", date: '10-04-2023', time: "12:26"}]
 return (
     <div className={styles.cont}>
         <h1><span>Н</span>овости</h1>
-        {sortedNewList.map((item,index) =>
-        <div className={styles.newsCont} key={index}>
-            <h3 className={styles.date}>{item.date}</h3>
-            {/* <p className={styles.time}>{item.time}</p>
-            <p className={styles.newsHeader}>{item.name}</p> */}
-            <div>
-                <p className={styles.time}>{item.time}</p>
-                <p className={styles.newsHeader}>{item.name}</p>
-            </div>
-        </div>
-        )}
-
-    </div>)
+        <News newsList={newsList} styles={styles}/>
+    </div>
+)
 }
 export default NewsPage
