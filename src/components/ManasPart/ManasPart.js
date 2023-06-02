fa6a7d212635b78e49d4400444e36c5e9488f027
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-06-02 22:22:01 +0600

    mainPAGE

--- src/components/ManasPart/ManasPart.js
+++ src/components/ManasPart/ManasPart.js
@@ -1,22 +1,34 @@
 import React from "react";
 import styles from "./ManasPart.module.css";
-import bookImg from "../../assests/images/MainPage/ManasPart/book_manas.png";
 import { NavLink } from "react-router-dom";
+import manas_left from "../../assests/images/MainPage/ManasPart/manasPart_left.png";
+import manas_right from "../../assests/images/MainPage/ManasPart/manasPart_right.png";
+import manas_book from "../../assests/images/MainPage/ManasPart/manas_book.png";
 
 const ManasPart = () => {
   return (
     <div className={styles.parent_manasPart}>
-      <div className={styles.parent_manasPart_shadow}>
-        <div className="container">
-          <div className={styles.child_manasPart}>
-            <div className={styles.child_manasPart_left}>
-              <img src={bookImg} alt="book" />
+      <h4>Манас</h4>
+      <div className={styles.child_manasPart}>
+        <div className={styles.child_manasPart_left}>
+          <img src={manas_left} alt="manas_left" />
+        </div>
+        <div className={styles.child_manasPart_center}>
+          <div>
+            <div>
+              <img src={manas_book} alt="manas_book" />
             </div>
-            <div className={styles.child_manasPart_right}>
-              <h3>Открой для себя Великий Эпос</h3>
-              <NavLink>Начните с первой страницы</NavLink>
-            </div>
+            <p>
+              Начните изучать и читать с легендарного , самого большого в мире
+              эпоса - “<span>МАНАС</span>”
+            </p>
           </div>
+          <button>
+            <NavLink to={"/"}>Подробнее</NavLink>
+          </button>
+        </div>
+        <div className={styles.child_manasPart_right}>
+          <img src={manas_right} alt="manas_right" />
         </div>
       </div>
     </div>
